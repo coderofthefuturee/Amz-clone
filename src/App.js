2972b8e722dc1f66c352851ecd2eaec131554099
Prince <princeking950@gmail.com>
2024-01-12 17:29:25 -0800

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={
+            <>
+              <Header />
+              <Checkout />
+            </>
+          } />
+          <Route path="/" element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
+  
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Header.css";
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <div className="header">
-            <div className='header_logo'>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <span class="material-symbols-outlined header_logoImage" fontSize="large">
-          storefront
-        </span>
-        <h2 className="header__logoTitle">AMZ</h2>
-      </div>
+      <div className="header">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className='header_logo'>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+          <span class="material-symbols-outlined header_logoImage" fontSize="large">
+            storefront
+          </span>
+          <h2 className="header__logoTitle">AMZ</h2>
+        </div>
+      </Link>
 
       <div className='header_search'>
         <input type="text" className="header__searchInput" />
@@ -32,13 +35,17 @@ function Header() {
           <span className="nav__itemLineTwo">Shop</span>
         </div>
 
-        <div className="nav__item">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-          <span class="material-symbols-outlined itemBasket">
-            shopping_cart
-          </span>
-          <span className="nav__itemLineTwo">0</span>
-        </div>
+        <Link to="/checkout" style={{ textDecoration: "none"}}>
+          <div className="nav__item">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+            <span class="material-symbols-outlined itemBasket">
+              shopping_cart
+            </span>
+            <span className="nav__itemLineTwo">0</span>
+          </div>
+        </Link>
+
+        
 
       </div>
     </div>
